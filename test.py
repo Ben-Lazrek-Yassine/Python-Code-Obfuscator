af9174ef05c2c2ddd16a23dcf0d62d3fffe131bc
Ben-Lazrek-Yassine <dogdevil181@gmail.com>
2022-10-30 12:28:46 +0100

    Add files via upload

---
+++ test.py
@@ -0,0 +1,21 @@
+import cmath
+import math
+
+a = 1
+b = 5
+k='string1'
+
+
+#life is nothing but pain and suffering
+aa=a+b
+c=k*b
+# blah blah blah comment 1
+def function1(x,y):
+	return x+y
+
+print('blah blah blah', function1(a,b))
+print(c)
+
+"llllllllllllllllll"
+
+"""aaaaaaaaaaaaaa"""
